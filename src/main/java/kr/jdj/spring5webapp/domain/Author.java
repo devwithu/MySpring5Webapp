b7fb68f29612953a811fce02317d00d3dba500e2
DaeJin Jeong <devwithu@naver.com>
2020-09-09 10:29:55 +0900

    add equels and hashCode

--- src/main/java/kr/jdj/spring5webapp/domain/Author.java
+++ src/main/java/kr/jdj/spring5webapp/domain/Author.java
@@ -1,10 +1,19 @@
 package kr.jdj.spring5webapp.domain;
 
+import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String firstName;
     private String lastName;
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
 
@@ -16,6 +25,14 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
@@ -40,5 +57,20 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
